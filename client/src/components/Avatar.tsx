12aa008ab9277e8555ac20875e808612b171cc41
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-05-27 16:38:24 -0700

    issues with remix and rbc

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,6 +1,9 @@
-import { ImageDescriptorProps } from "../vite-env";
+interface AvatarProps {
+  url: string;
+  alt_text: string;
+}
 
-export default function Avatar(props: ImageDescriptorProps) {
+export default function Avatar(props: AvatarProps) {
   return (
     <div className="flex -space-x-1 overflow-hidden">
       <img className="rounded-full w-8" src={props.url} alt={props.alt_text} />
